1e8503f9cf0d098dfc0799ded8d8ef94260011ae
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2021-03-25 14:45:32 -0300

    Fix to jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
