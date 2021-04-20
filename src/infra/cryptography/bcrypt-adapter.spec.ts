160365a08e82b410707db8c8bc49aa5735641a65
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2021-04-20 17:32:59 -0300

    Adding bcrypt

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,26 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+    async hash (): Promise<string> {
+        return new Promise(resolve => resolve('hash'))
+    }
+}))
+
+describe('Bcrypt Adapter', () => {
+    test('Shoul call bcrypt with correct value', async() => {
+        const salt = 12
+        const sut = new BcryptAdapter(salt)
+        const hashSpy = jest.spyOn(bcrypt, 'hash')
+        await sut.encrypt('any_value')
+
+        expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+    test('Shoul return a hash on success', async() => {
+        const salt = 12
+        const sut = new BcryptAdapter(salt)
+        const hash = await sut.encrypt('any_value')
+
+        expect(hash).toBe('hash')
+    })
+})
