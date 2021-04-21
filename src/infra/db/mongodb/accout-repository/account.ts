68d729e813c1bc78dab44c2256fb26ef5150ebd5
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2021-04-21 13:16:54 -0300

    mongodb

---
+++ src/infra/db/mongodb/accout-repository/account.ts
@@ -0,0 +1,12 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AddAcountModel } from '../../../../domain/usecases/add-acount'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helper/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAcountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    return MongoHelper.map(result.ops[0])
+  }
+}
---
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -0,0 +1,25 @@
+import { MongoClient, Collection } from 'mongodb'
+import { AccountModel } from '../../../../domain/models/account'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect(url: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  },
+  async disconnect(): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
+  },
+  
+   map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, {id: _id})
+}
+}
