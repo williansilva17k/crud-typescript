f7c6eec354384fb60ae52d2d79383809a3de7057
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2021-04-23 12:42:35 -0300

    fix

--- src/main/adapters/express-routes-adapter.ts
+++ src/main/adapters/express-routes-adapter.ts
@@ -6,8 +6,13 @@ export const adaptRoute = (controller: Controller) => {
         const httpRequest: HttpRequest = {
             body: req.body
         }
-
         const httpResponse = await controller.handle(httpRequest)
-        res.status(httpResponse.statusCode).json(httpRequest.body)
+        if(httpResponse.statusCode == 200){
+            res.status(httpResponse.statusCode).json(httpRequest.body)
+        } else {
+            res.status(httpResponse.statusCode).json({
+                error: httpRequest.body.message
+            })
+        }
     }
 } 
