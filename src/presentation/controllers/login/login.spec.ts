e4cbdb55b678e6706ee44d1014150cd8f77bf254
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2021-04-23 13:11:27 -0300

    fix to logincontroller

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,21 +1,41 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
+import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
 import { LoginController } from './login'
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password',
+  },
+})
 
 interface SutTypes {
   sut: LoginController
+  emailValidatorStub: EmailValidator
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController()
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new LoginController(emailValidatorStub)
   return {
     sut,
+    emailValidatorStub,
   }
 }
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const { sut }= makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password',
@@ -26,7 +46,8 @@ describe('Login Controller', () => {
       badRequest(new MissingParamError('email'))
     )
   })
-  test('Should return 400 if no email is provided', async () => {
+
+  test('Should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -37,5 +58,21 @@ describe('Login Controller', () => {
     expect(httpResponse.statusCode).toEqual(
       badRequest(new MissingParamError('password'))
     )
+  })
+
+  test('Should return 400 an invalid email is provided', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse.statusCode).toEqual(
+      badRequest(new InvalidParamError('email'))
+    )
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    await sut.handle(makeFakeRequest())
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,14 +1,26 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
-        return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
-    } 
-    if (!httpRequest.body.password) {
-        return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
+      const { email, password } = httpRequest.body
+    if (!email) {
+      return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
+    }
+    if (!password) {
+      return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
+    }
+    const isValid = this.emailValidator.isValid(email)
+    if(!isValid) {
+        return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))))
     }
   }
 }
