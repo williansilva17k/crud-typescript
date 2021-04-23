7791654e8e1ab4df1c6b55425c4328574e685db7
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2021-04-23 12:56:46 -0300

    add LoginController

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,14 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LoginController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+        return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
+    } 
+    if (!httpRequest.body.password) {
+        return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
+    }
+  }
+}
