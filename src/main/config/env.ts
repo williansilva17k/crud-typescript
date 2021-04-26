dff07f4c8484d0129ed5beb1b6e14dc65762f5c0
Willian Evangelista <willianevangelistasilva17@gmail.com>
2021-04-26 15:42:35 -0300

    fix: fixes

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://admin:<Casemods13@>@cluster0.4sv9w.mongodb.net/v1?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/v1',
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || 'tj67O==5H'
 }
